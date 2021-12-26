83219f52755c612713855f1b2cabfa15783aedc9
Martin Masevski <martin.masevski@gmail.com>
2021-12-26 15:26:51 +0100

    initial commit

---
+++ components/HomeCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img :src="home.images[0]" style="width: 200px;"> <br/>
+            <home-card :home="home"></home-card>
+    </div>
+</template>
+
+<script>
+export default{
+    props:{
+        home: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
